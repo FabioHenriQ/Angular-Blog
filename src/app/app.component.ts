4a3efb9d2c055da7ba477b2fc212a68ad489112b
Fabio Henrique <Farabio973@hotmail.com>
2024-02-01 20:11:32 -0300

    Menu and card html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BigCardComponent } from './components/big-card/big-card.component';
-import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
-import { RecentSectionComponent } from './components/recent-section/recent-section.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BigCardComponent, MenuBarComponent, RecentSectionComponent, FooterComponent],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
