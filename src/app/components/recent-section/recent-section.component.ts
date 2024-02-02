6cde35e8fa0494fa7e3ca3d6a9b8b72672df137b
Fabio Henrique <Farabio973@hotmail.com>
2024-02-02 14:05:27 -0300

    Finished the automatic news renderer

--- src/app/components/recent-section/recent-section.component.ts
+++ src/app/components/recent-section/recent-section.component.ts
@@ -10,4 +10,22 @@ import { SmallCardComponent } from '../small-card/small-card.component';
 })
 export class RecentSectionComponent {
 
+  recents = {
+    news: [{
+      id: 0,
+      title: "noticia 1",
+      img: "https://th.bing.com/th/id/OIP.48USig7crJ7H-Lui59isuAHaEo?rs=1&pid=ImgDetMain",
+      description: "Sla sla sla",
+      namePerson: "Fabio",
+      imgPerson: "https://th.bing.com/th/id/OIP.xo-BCC1ZKFpLL65D93eHcgHaGe?rs=1&pid=ImgDetMain"
+    },
+    {
+      id: 1,
+      title: "noticia 2",
+      img: "https://th.bing.com/th/id/OIP.48USig7crJ7H-Lui59isuAHaEo?rs=1&pid=ImgDetMain",
+      description: "Sla sla sla",
+      namePerson: "Luick",
+      imgPerson: "https://th.bing.com/th/id/OIP.xo-BCC1ZKFpLL65D93eHcgHaGe?rs=1&pid=ImgDetMain"
+    }]
+  }
 }
