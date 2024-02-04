9c4fc10668419cfe0dff72be4dad33ff43cbdf18
Fabio Henrique <Farabio973@hotmail.com>
2024-02-03 22:46:50 -0300

    Added more images and fixed layout

--- src/app/components/recent-section/recent-section.component.ts
+++ src/app/components/recent-section/recent-section.component.ts
@@ -13,28 +13,28 @@ export class RecentSectionComponent {
   recents = {
     news: [{
       title: "noticia 1",
-      img: "https://th.bing.com/th/id/OIP.48USig7crJ7H-Lui59isuAHaEo?rs=1&pid=ImgDetMain",
+      img: "/assets/images/robot.jpg",
       description: "Sla sla sla",
       namePerson: "Fabio",
       imgPerson: "https://th.bing.com/th/id/OIP.xo-BCC1ZKFpLL65D93eHcgHaGe?rs=1&pid=ImgDetMain"
     },
     {
       title: "noticia 2",
-      img: "https://th.bing.com/th/id/OIP.48USig7crJ7H-Lui59isuAHaEo?rs=1&pid=ImgDetMain",
+      img: "/assets/images/home-office.jpg",
       description: "Sla sla sla",
       namePerson: "Luick",
       imgPerson: "https://th.bing.com/th/id/OIP.xo-BCC1ZKFpLL65D93eHcgHaGe?rs=1&pid=ImgDetMain"
     },
     {
       title: "noticia 3",
-      img: "https://th.bing.com/th/id/OIP.48USig7crJ7H-Lui59isuAHaEo?rs=1&pid=ImgDetMain",
+      img: "/assets/images/programming.jpg",
       description: "Sla sla sla",
       namePerson: "Luick",
       imgPerson: "https://th.bing.com/th/id/OIP.xo-BCC1ZKFpLL65D93eHcgHaGe?rs=1&pid=ImgDetMain"
     },
     {
       title: "noticia 4",
-      img: "https://th.bing.com/th/id/OIP.48USig7crJ7H-Lui59isuAHaEo?rs=1&pid=ImgDetMain",
+      img: "/assets/images/social-medias.jpg",
       description: "Sla sla sla",
       namePerson: "Luick",
       imgPerson: "https://th.bing.com/th/id/OIP.xo-BCC1ZKFpLL65D93eHcgHaGe?rs=1&pid=ImgDetMain"
