5e9c4256b38d2e9462e439eb47fda6ff334f8d6c
Fabio Henrique <Farabio973@hotmail.com>
2024-02-15 22:31:03 -0300

    Added more routes

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
